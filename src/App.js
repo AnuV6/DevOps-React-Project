00851adb0fe412adcbf5462e691eaa0ab8d7b242
Anupa <anupa@gmail.com>
2025-03-07 21:46:46 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
-      <b><p>I created react app with CI/CD pipeline. 
-        I have used Jenkins for CI/CD pipeline.
-        I have used Docker for containerization.
+      <b><p>I created a react app with CI/CD pipeline. 
+        I have used Jenkins for CI/CD pipeline and Docker for containerization.
       </p></b>
     </div>
   );
