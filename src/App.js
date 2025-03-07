05fa031a332bccb7f300bc7cbf47a0ad092b1de8
Anupa <anupa@gmail.com>
2025-03-07 20:26:05 +0530

    Add initial React app setup with Docker, testing, and essential files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <b><p>I created react app with CI/CD pipeline. 
+        I have used Jenkins for CI/CD pipeline.
+        I have used Docker for containerization.
+      </p></b>
+    </div>
+  );
+}
+
+export default App;
